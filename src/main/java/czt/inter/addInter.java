a8c0258ad4d656fe5caaf13941facc0632e3851c
wxMaiCoder <343504851@qq.com>
2019-07-24 22:52:27 +0800

    jar包有问题

--- src/main/java/czt/inter/addInter.java
+++ src/main/java/czt/inter/addInter.java
@@ -1,28 +1,51 @@
 package czt.inter;
 
+
+
+import czt.vo.User;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintWriter;
-
 
 
 @WebServlet(name = "addInter")
 public class addInter extends HttpServlet {
 
+    private User buildUserDO(){
+        User user = new User();
+        user.setName("张三");
+        user.setPassword("123");
+        user.setAge("11");
+
+
+        return user;
+    }
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doGet(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //设置网页响应类型
-        response.setContentType("text/html");
-        //实现具体操作
+        System.out.println(1);
+        response.setContentType("application/json; charset=utf-8");
+        response.setCharacterEncoding("UTF-8");
+        //阿里巴巴这个包有问题
+       // String userJson = JSON.toJSONString(buildUserDO());
+
+        //OutputStream out = response.getOutputStream();
+        //out.write(userJson.getBytes("UTF-8"));
         PrintWriter out = response.getWriter();
-        out.println("This is a new servlet page");
+        String str ="{\"姓名\":\"HaHa先生\",\"年龄\":\"18岁啦\"}";
+        out.println(str);
+        out.flush();
+        System.out.println(2);
+
     }
 
 }
---
+++ src/main/java/czt/vo/User.java
@@ -0,0 +1,33 @@
+package czt.vo;
+
+import java.util.Date;
+
+public class User {
+    private String name ;
+    private String age;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
